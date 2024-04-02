0fab86af743a1aae0a960d279ce0d1c7b5e54444
deborahambelo <ketema.deborah@gmail.com>
2024-04-02 02:21:53 -0700

    added file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if my_list:
-        if idx < 0 or idx >= len(my_list):
-            return None
-        else:
-            return my_list[idx]
-    else:
-        return None
+    if idx < 0:
+        return (None)
+
+    length = len(my_list)
+
+    if idx > length - 1:
+        return (None)
+
+    return(my_list[idx])
