bf3fe576fb4d8eda669e9d6b8c1451e34d5cec79
deborahambelo <ketema.deborah@gmail.com>
2024-04-05 10:56:47 -0700

    my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,3 @@
-#!/usr/bin/python3
-"""
-contains the MyList class
-"""
-
-
 class MyList(list):
     """a subclass of list"""
     def __init__(self):
@@ -13,3 +7,4 @@ class MyList(list):
     def print_sorted(self):
         """prints the sorted list"""
         print(sorted(self))
+
