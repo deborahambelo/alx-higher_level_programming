aac2d472ff0628bdb26f0e3e405ea49195b95cdd
deborahambelo <ketema.deborah@gmail.com>
2024-04-11 06:44:42 -0700

    task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-      const Rectangle = {};
-      return `${Rectangle}`;
-    } else {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+    } else {
+      const Rectangle = {};
+      return `${Rectangle}`;
     }
   }
 }
