2a8bfef652ebedc2fd753c14fa850b5d6b662437
deborahambelo <ketema.deborah@gmail.com>
2024-04-12 01:13:09 -0700

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    } else {
+      const Rectangle = {};
+      return `${Rectangle}`;
+    }
+  }
+
+  print () {
+    let i = 0;
+    for (i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+}
+module.exports = Rectangle;
