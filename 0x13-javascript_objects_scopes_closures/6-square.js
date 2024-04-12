2057f88d0c6ea232e87162309d2ade24d57f439c
deborahambelo <ketema.deborah@gmail.com>
2024-04-12 03:46:39 -0700

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -13,7 +13,7 @@ class Square extends Sq {
       for (i = 0; i < this.height; i++) {
         let line = '';
         for (let j = 0; j < this.width; j++) {
-          line += 'C';
+          line += c;
         }
         console.log(line);
       }
