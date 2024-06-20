840d5a7d628fe0b23967e08c57ddad2d344e6d3f
deborahambelo <101412212+deborahambelo@users.noreply.github.com>
2024-06-20 21:11:18 +0530

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import urllib.request
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-a = type(html)
-b = html.decode("utf-8")
-print("Body response:")
-print("\t- type: {}".format(a))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(b))
+"""Script that fetches https://alx-intranet.hbtn.io/status."""
+from urllib.request import Request, urlopen
+
+
+if __name__ == "__main__":
+    req = Request("https://alx-intranet.hbtn.io/status")
+    with urlopen(req) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
